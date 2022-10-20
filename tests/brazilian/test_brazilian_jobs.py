4798c9d8c51e14e5258e7b6a78474d334cd53c89
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-20 18:22:36 -0300

    test: Requisito 11 - criado teste para função "read_brazilian_file"
    verifica se na lista retorna, possui dicionarios com chaves corretas

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for element in brazilian_jobs:
+        assert ('title' in element) is True
+        assert ('salary' in element) is True
+        assert ('type' in element) is True
