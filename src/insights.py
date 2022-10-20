187046bf942e3cf285ebd0b4ae1f25627ffbe45b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-20 15:02:44 -0300

    feat: Requisito 2 - criada função "get_unique_job_types" para retornar uma lista de tipos de jobs

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs_list = read(path)
+    job_type_set = set()
+    for job in jobs_list:
+        job_type_set.add(job['job_type'])
+    return list(job_type_set)
 
 
 def filter_by_job_type(jobs, job_type):
